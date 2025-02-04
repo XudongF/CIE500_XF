c18932e7b28bc43863efbab44c0e183480224c57
Xudong Fan <xudongfa@buffalo.edu>
2025-02-04 16:21:05 -0500

    correct the adjancecy matrix issue

--- week3/random_network.py
+++ week3/random_network.py
@@ -9,11 +9,11 @@ import numpy as np
 
 ## First, we generate 8 random nodes in the horizontal space (0,2) and vertical space (0,4)
 random.seed(66)
-pos = [(random.random() * 2.0, random.random() * 3.0) for _ in range(8)]
+pos = {i: (random.random() * 2.0, random.random() * 3.0) for i in range(8)}
 
 ## Second, we create an edge list by a given probability.
 edge_list = []
-for node_pair in combinations(list(range(8)), 2):
+for node_pair in combinations(list(pos.keys()), 2):
     exist_prob = random.random()
     if exist_prob > 0.7:
         edge_list.append(node_pair)
@@ -53,7 +53,7 @@ def bmatrix(
     return "\n".join(rv)
 
 
-A = nx.adjacency_matrix(G).toarray()
+A = nx.adjacency_matrix(G, nodelist=list(pos.keys())).toarray()
 
 print(f"The latex version of adjacency matrix is \n {bmatrix(A)}")
 
@@ -75,8 +75,11 @@ plt.savefig(
     bbox_inches="tight",
 )
 
-# Finally, we can get the edgelist and adjancency matrix from Graph directly.
+# Finally, we can get the edgelist and adjancency matrix from Graph directly. We need to define the node sequence by
+# feeding the nodelist option.
 
-print(f"The adjancency matrix of G is \n {nx.adjacency_matrix(G).toarray()}")
+print(
+    f"The adjancency matrix of G is \n {nx.adjacency_matrix(G, nodelist=list(pos.keys())).toarray()}"
+)
 
 print(f"The edge list of G is \n {nx.to_edgelist(G)}")
